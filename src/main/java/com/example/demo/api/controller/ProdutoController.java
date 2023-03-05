e2da598098b6eebfeb40ae89e31a5be3846d3966
eparizoto <eparizoto@gmail.com>
2023-03-05 18:46:15 -0300

    Configuração OpenApi

--- src/main/java/com/example/demo/api/controller/ProdutoController.java
+++ src/main/java/com/example/demo/api/controller/ProdutoController.java
@@ -1,14 +1,7 @@
 package com.example.demo.api.controller;
 
-import com.example.demo.api.handlers.model.ApiErrorResponse;
 import com.example.demo.core.ProdutoService;
 import com.example.demo.model.Produtos;
-import io.swagger.annotations.Api;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -18,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@Api(tags = "Produtos")
+
 @RestController
 @RequestMapping("/v1/produto")
 @Log4j2
@@ -27,17 +20,7 @@ public class ProdutoController {
     @Autowired
     ProdutoService produtoService;
 
-    @Operation(description = "Retorna os produtos")
-    @ApiResponses(
-            value = {
-                    @ApiResponse(
-                            responseCode = "200",
-                            description = "Retorna a relação de produtos"),
-                    @ApiResponse(
-                            responseCode = "400",
-                            description = "Bad Request. Dados de entrada inválidos",
-                            content = @Content(schema = @Schema(implementation = ApiErrorResponse.class)))
-            })
+
     @GetMapping(value = "/produtos", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Produtos> listarProdutos(@RequestParam(value = "loja") Integer loja, @RequestParam(value = "sku") Long sku){
 
--- src/main/java/com/example/demo/core/ProdutoService.java
+++ src/main/java/com/example/demo/core/ProdutoService.java
@@ -1,6 +1,5 @@
 package com.example.demo.core;
 
-import com.example.demo.api.exceptions.ProdutoValidationException;
 import com.example.demo.model.Produto;
 import com.example.demo.model.Produtos;
 import org.springframework.stereotype.Service;
@@ -16,8 +15,8 @@ public class ProdutoService {
                 Produto.builder().id(2).descricao("Água Sanitária").build(),
                 Produto.builder().id(3).descricao("Detergente").build());
 
-        throw new ProdutoValidationException("Erro");
+        //throw new ProdutoValidationException("Erro");
 
-        //return Produtos.builder().produtos(produtos).build();
+        return Produtos.builder().produtos(produtos).build();
     }
 }
