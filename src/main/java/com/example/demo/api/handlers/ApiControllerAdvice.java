bca8a567952df77be2c34bf156c29dc7ebd94b0c
eparizoto <eparizoto@gmail.com>
2023-03-05 21:23:12 -0300

    Configuração Token

--- src/main/java/com/example/demo/api/handlers/ApiControllerAdvice.java
+++ src/main/java/com/example/demo/api/handlers/ApiControllerAdvice.java
@@ -2,16 +2,25 @@ package com.example.demo.api.handlers;
 
 import com.example.demo.api.exceptions.ProdutoValidationException;
 import com.example.demo.api.handlers.model.ApiErrorResponse;
+import com.example.demo.core.exceptions.externalservices.UnauthorizedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
 
 import java.time.LocalDateTime;
 
 @ControllerAdvice
 public class ApiControllerAdvice {
+
+    @ResponseBody
+    @ExceptionHandler(UnauthorizedException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    ApiErrorResponse unauthorizedErrorHandler(Exception e, WebRequest request) {
+        return construirRespostaErro(HttpStatus.UNAUTHORIZED, e);
+    }
 
     @ResponseBody
     @ExceptionHandler({
---
+++ src/main/java/com/example/demo/api/token/TokenInterceptorConfig.java
@@ -0,0 +1,35 @@
+//package com.example.demo.api.token;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+///**
+// * Classe configuradora do TokenInterceptor
+// */
+//@Configuration
+//public class TokenInterceptorConfig implements WebMvcConfigurer {
+//
+//    /***
+//     * Método que adiciona o interceptor e exclui do interceptor os paths passados como parâmetros.
+//     * Aqui estamos liberando os paths do Swagger e documentação da API.
+//     * @param registry InterceptorRegistry a ser configurado.
+//     */
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(getInterceptor())
+//                .excludePathPatterns(
+//                        "/swagger-resources/configuration/*",
+//                        "/swagger-ui/*",
+//                        "/swagger-resources",
+//                        "/v2/api-docs",
+//                        "/v1/login",
+//                        "/v1/login/refresh-token");
+//    }
+//
+//    @Bean
+//    public TokenInterceptor getInterceptor(){
+//        return new TokenInterceptor();
+//    }
+//}
