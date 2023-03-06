2d0c9891ec5e634773aae359a7f685b7301481a8
eparizoto <eparizoto@gmail.com>
2023-03-05 23:03:00 -0300

    Autenticando com token

--- src/main/java/com/example/demo/api/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/api/config/OpenApiConfig.java
@@ -1,8 +1,11 @@
 package com.example.demo.api.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,6 +18,9 @@ class OpenApiConfig {
                                  String appDesciption,
                                  @Value("${application-version}")
                                  String appVersion) {
+
+        final String securitySchemeName = "bearerAuth";
+
         return new OpenAPI()
                 .info(new Info()
                         .title("Projeto demo OpenApi3.0")
@@ -23,6 +29,16 @@ class OpenApiConfig {
                         .termsOfService("http://swagger.io/terms/")
                         .license(new License().
                                 name("Apache 2.0").
-                                url("http://springdoc.org")));
+                                url("http://springdoc.org")))
+                .addSecurityItem(new SecurityRequirement()
+                        .addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .description(
+                                        "Provide the JWT token. JWT token can be obtained from the Login API. For testing, use the credentials <strong>john/password</strong>")
+                                .bearerFormat("JWT")));
     }
 }
--- src/main/java/com/example/demo/api/controller/ProdutoController.java
+++ src/main/java/com/example/demo/api/controller/ProdutoController.java
@@ -2,6 +2,7 @@ package com.example.demo.api.controller;
 
 import com.example.demo.core.ProdutoService;
 import com.example.demo.model.Produtos;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/v1/produto")
 @Log4j2
+@SecurityRequirement(name = "bearerAuth") //Autenticação a nivel de classe
 public class ProdutoController {
 
     @Autowired
